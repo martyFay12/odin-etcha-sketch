2bba905b60f82e04530cd90340d9e488e3bec763
Marty Fay <marty.r.fay@gmail.com>
2023-05-18 11:00:41 -0300

    Create function to prompt user for new grid size

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const mainContainer = document.querySelector(".main-container");
 createEtchaSketch(10);
 const boxes = document.querySelectorAll(".box");
 boxes.forEach((box) => box.addEventListener("mouseover", addInkClass));
+const resizeGridButton = document.querySelector("#change-grid");
+resizeGridButton.addEventListener("click", newGrid);
 
 function createEtchaSketch(gridSize) {
   for (let i = 0; i < gridSize; i++) {
@@ -18,4 +20,19 @@ function createEtchaSketch(gridSize) {
 
 function addInkClass(event) {
   this.classList.add("inked");
+}
+
+function newGrid(event) {
+  const gridSize = newGridSize();
+}
+function newGridSize() {
+  let gridSize = parseInt(prompt("Enter a new grid size between 10 and 100"));
+  while (gridSize < 10 || gridSize > 100 || isNaN(gridSize)) {
+    gridSize = parseInt(
+      prompt(
+        "not a valid grid size, please enter a valid one between 10 and 100."
+      )
+    );
+  }
+  return gridSize;
 }
