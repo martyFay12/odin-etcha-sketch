07f2aea5425330b4e0d1e09e15b18cb81ac2620d
Marty Fay <marty.r.fay@gmail.com>
2023-05-18 10:47:52 -0300

    Complete 'drawing' part of the etcha sketch.

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const mainContainer = document.querySelector(".main-container");
-createEtchaSketch();
-function createEtchaSketch() {
-  for (let i = 0; i < 16; i++) {
+createEtchaSketch(10);
+const boxes = document.querySelectorAll(".box");
+boxes.forEach((box) => box.addEventListener("mouseover", addInkClass));
+
+function createEtchaSketch(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
     const horizontalDiv = document.createElement("div");
     horizontalDiv.classList.add("row");
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < gridSize; j++) {
       const boxDiv = document.createElement("div");
       boxDiv.classList.add("box");
       horizontalDiv.appendChild(boxDiv);
     }
     mainContainer.appendChild(horizontalDiv);
   }
+}
+
+function addInkClass(event) {
+  this.classList.add("inked");
 }
